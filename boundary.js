a1e23202a96e1f15ea34cf18b1def868b96986d7
Jamie Daniel Meredith Gardner <jgardner05@qub.ac.uk>
2019-05-09 13:29:53 +0100

    Visual instructions

--- boundary.js
+++ boundary.js
@@ -29,34 +29,34 @@ class Boundary {
   static pentagon() {
     // 1
     let ax1 = w * 0.1;
-    let ay1 = h * 2 / 5;
-    let ax2 = w * 0.6;
-    let ay2 = h * 0.06;
+    let ay1 = h * 2 / 7;
+    let ax2 = w * 0.5;
+    let ay2 = h * 0.1;
     // 2
-    let bx1 = w * 0.52;
-    let by1 = h * 0.15;
+    let bx1 = w * 0.5;
+    let by1 = h * 0.1;
     let bx2 = w * 0.9;
-    let by2 = w * 0.5;
+    let by2 = h * 2 / 7;
     // 3
-    let cx1 = w * 0.95;
-    let cy1 = h * 0.45;
-    let cx2 = w / 2;
-    let cy2 = w / 2;
+    let cx1 = w * 0.9;
+    let cy1 = h * 2 / 7;
+    let cx2 = w * 4 / 5;
+    let cy2 = h * 0.9;
     // 4
-    let dx1 = w / 2;
-    let dy1 = w / 2;
-    let dx2 = w / 2;
-    let dy2 = w / 2;
+    let dx1 = w * 4 / 5;
+    let dy1 = h * 0.9;
+    let dx2 = w * 1 / 5;
+    let dy2 = h * 0.9;
     // 5
-    let ex1 = w / 2;
-    let ey1 = w / 2;
-    let ex2 = w / 2;
-    let ey2 = w / 2;
+    let ex1 = w * 1 / 5;
+    let ey1 = h * 0.9;
+    let ex2 = w * 0.1;
+    let ey2 = h * 2 / 7;
 
-    walls[0] = new Boundary(ax1, ay1, ax2, ay2);
-    walls[1] = new Boundary(bx1, by1, bx2, by2);
-    walls[2] = new Boundary(cx1, cy1, cx2, cy2);
-    walls[3] = new Boundary(dx1, dy1, dx2, dy2);
-    walls[4] = new Boundary(ex1, ey1, ex2, ey2);
+    walls[0] = new Boundary(ax1 * random(0.8, 1.2), ay1 * random(0.8, 1.2), ax2 * random(0.8, 1.2), ay2 * random(0.8, 1.2));
+    walls[1] = new Boundary(bx1 * random(0.8, 1.2), by1 * random(0.8, 1.2), bx2 * random(0.8, 1.2), by2 * random(0.8, 1.2));
+    walls[2] = new Boundary(cx1 * random(0.8, 1.2), cy1 * random(0.8, 1.2), cx2 * random(0.8, 1.2), cy2 * random(0.8, 1.2));
+    walls[3] = new Boundary(dx1 * random(0.8, 1.2), dy1 * random(0.8, 1.2), dx2 * random(0.8, 1.2), dy2 * random(0.8, 1.2));
+    walls[4] = new Boundary(ex1 * random(0.8, 1.2), ey1 * random(0.8, 1.2), ex2 * random(0.8, 1.2), ey2 * random(0.8, 1.2));
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,35 +8,75 @@ let ray;
 let xoff = 0;
 let yoff = 1000;
 
+let fontOpacity;
+let fontSize;
+let isActive;
+let currentFrame;
+
 function setup() {
   w = windowWidth;
   h = windowHeight;
   createCanvas(w, h);
 
-
   particle = new Particle();
-  Boundary.randomWalls(5);
-  // Boundary.pentagon();
+
+  // Boundary.randomWalls(5);
+  Boundary.pentagon();
+
   // Boundary.showWalls();
+
+  fontOpacity = 0;
+  fontSize = w * 0.018;
+  isActive = false;
+  currentFrame = 0;
 }
 
 function draw() {
   background(30);
 
+  // Walls
   for (let wall of walls) {
     wall.show();
   }
 
+  // Dot
   particle.update(mouseX, mouseY);
   // particle.update(noise(xoff) * width, noise(yoff) * height);
   particle.show(walls);
 
   xoff += 0.001;
   yoff += 0.001;
+
+  // Instructions
+  let limit = 60;
+  if (!isActive) {
+    if (fontSize < w * 0.02) {
+      fontSize += w * 0.0001;
+    }
+    fontOpacity += 15;
+  } else if (isActive && frameCount > currentFrame + limit) {
+    fontOpacity -= 15;
+    if (fontSize < w * 0.023) {
+      fontSize += w * 0.00005;
+    }
+  }
+
+  fill(255, fontOpacity);
+  textSize(fontSize);
+  textAlign(CENTER, CENTER);
+  text("Move the mouse", w / 2, h / 2);
 }
 
 function windowResized() {
   w = windowWidth;
   h = windowHeight;
   resizeCanvas(w, h);
+}
+
+function mouseMoved() {
+  if (!isActive) {
+    currentFrame = frameCount;
+    isActive = true;
+  }
+  return false;
 }
